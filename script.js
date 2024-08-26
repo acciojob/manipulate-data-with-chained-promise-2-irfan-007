a821fd5b0ebe4452723b66bb3e68bf5b59187bd2
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-27 00:04:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getArray(){
+    return new Promise((res,rej)=>{
+    setTimeout(()=>res([1,2,3,4]),3000);
+    });
+}
+
+const output=document.getElementById("output");
+
+getArray()
+.then((arr)=>arr.filter(k=>k%2==0))
+.then((arr)=>arr.map(k=>k*2))
+.then((result)=>output.innerText=result);
