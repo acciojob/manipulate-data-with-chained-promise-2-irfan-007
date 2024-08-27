4b22f723c7893077aff6580f5294c2baf1fa0a29
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-27 21:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 //your JS code here. If required.
-function getArray(){
-    return new Promise((res,rej)=>{
-    setTimeout(()=>res([1,2,3,4]),3000);
-    });
+const output = document.getElementById("output");
+function getArray() {
+  return new Promise((res, rej) => {
+    let arr = [1, 2, 3, 4].filter((k) => k % 2 == 0);
+    setTimeout(() => res(arr), 1000);
+  });
+} 
+function getMult(ar) {
+  console.log(ar);
+
+  return new Promise((res, rej) => {
+    let arr = ar.map((k) => k * 2);
+    setTimeout(() => res(arr), 2000);
+  });
 }
 
-const output=document.getElementById("output");
-
 getArray()
-.then((arr)=>arr.filter(k=>k%2==0))
-.then((arr)=>arr.map(k=>k*2))
-.then((result)=>output.innerText=result);
+  .then((ans) => {
+    output.innerHTML = ans;
+    return ans;
+  })
+  .then(getMult)
+  .then((result) => {
+    output.innerHTML = result;
+  });
